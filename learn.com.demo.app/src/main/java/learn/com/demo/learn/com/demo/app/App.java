046edb5d9d2b4bd87d565c1e3ba2758e53afdafa
Adarsha H K <adarshahk2142002@gmail.com>
2024-02-21 12:07:13 +0530

    commit git

---
+++ learn.com.demo.app/src/main/java/learn/com/demo/learn/com/demo/app/App.java
@@ -0,0 +1,13 @@
+package learn.com.demo.learn.com.demo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
